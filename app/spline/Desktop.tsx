89e186a4b69886f5ae419a013ba43b0a7d3235b5
eng10 <cabdilahi0099@gmail.com>
2024-12-10 00:03:27 +0300

    SEO fix

--- app/spline/Desktop.tsx
+++ app/spline/Desktop.tsx
@@ -1,13 +1,13 @@
-'use client'
-import Spline from '@splinetool/react-spline/next';
+// 'use client'
+// import Spline from '@splinetool/react-spline/next';
 
-const  Desktop=()=> {
-  return (
-    <main>
-      <Spline
-        scene="loading..." 
-      />
-    </main>
-  );
-}
-export default Desktop
+// const  Desktop=()=> {
+//   return (
+//     <main>
+//       <Spline
+//         scene="loading..." 
+//       />
+//     </main>
+//   );
+// }
+// export default Desktop
